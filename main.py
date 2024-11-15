9a93ea51820c0aa18055a2659049656e6f7cccc9
Holo <dudorov_dm@mail.ru>
2024-11-15 23:09:24 +0500

    start server fix4

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-import os
+import uvicorn
 
 app = FastAPI()
 
-
+# Монтируем статические файлы
+app.mount("/style", StaticFiles(directory="style"), name="style")
+app.mount("/source", StaticFiles(directory="source"), name="source")
 
 # Маршрут для обслуживания index.html
 @app.get("/", response_class=HTMLResponse)
 async def read_index():
-    with open("pages/index.html", "r") as file:
-        return HTMLResponse(content=file.read())
-@app.get("/page", response_class=HTMLResponse)
-async def read_index():
-    with open("pages/index.html", "r") as file:
+    with open("pages/index.html", "r", encoding="utf-8") as file:
         return HTMLResponse(content=file.read())
 
+@app.get("/page", response_class=HTMLResponse)
+async def read_page():
+    with open("pages/index.html", "r", encoding="utf-8") as file:
+        return HTMLResponse(content=file.read())
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
